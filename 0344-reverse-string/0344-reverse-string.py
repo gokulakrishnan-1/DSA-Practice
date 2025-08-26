52d7b38fe96ef63cbce4fd30519722ec3076a4d2
Gokulakrishnan V <122203422+gokulakrishnan-1@users.noreply.github.com>
2025-08-26 21:04:02 +0530

    Time: 3 ms (41.32%), Space: 23 MB (83.21%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,12 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        stack = []
-        for c in s:
-            stack.append(c)
         
-        i = 0
-        for i in range(len(s)):
-            s[i] = stack.pop()
-            i += 1
-        
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
         return s
